06ddd6d498fe3201a73c94f33bfe881a9f681dd7
Maria Huaccha <maria.huaccha@koronet.com>
2025-08-29 02:18:37 +0000

    feat: ajustes UI Open Discovery + fix CTA Shop Now

--- .next/static/chunks/src_app_catalog_layout_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_6d93a6aa._.js",
-  "static/chunks/node_modules_a1f6ece7._.js"
-],
-    source: "dynamic"
-});
